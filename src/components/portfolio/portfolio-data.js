f9729f162db2d7466d8ad1d21162fb8871dd2e0a
macjonessoriano <sorianomacjones@gmail.com>
2023-04-05 17:34:12 +0800

    change projects

--- src/components/portfolio/portfolio-data.js
+++ src/components/portfolio/portfolio-data.js
@@ -16,27 +16,28 @@ export const portfolioData = [
 	},
 	{
 		id: 2,
-		image: IMG4,
-		title: 'Pig Game',
-		description: 'Pig Game using Javascript, HTML, and CSS',
-		github: 'https://github.com/Mac-Jones/pig-game-javascript',
-		demo: 'https://pig-game-mac-jones.netlify.app/',
-	},
-	{
-		id: 3,
-		image: IMG5,
-		title: 'Breakout',
-		description: 'Javascript Game using HTML, CSS and Javascript',
-		github: 'https://github.com/Mac-Jones/breakout-game-javascript',
-		demo: 'https://breakout-macjones.netlify.app/',
-	},
-	{
-		id: 4,
 		image: IMG2,
 		title: 'Crown Clothing',
 		description: 'E-Commerce App using React, TypeScript, Redux, etc',
 		github: 'https://github.com/Mac-Jones/Crown-Clothing',
 		demo: 'https://crown-clothing-reactjs.netlify.app/',
+	},
+	{
+		id: 3,
+		image: IMG6,
+		title: 'My Personal Blog',
+		description: 'Simple Blog using Gatsby',
+		github: 'https://github.com/Mac-Jones/gatsby-blog-mj',
+		demo: 'https://macjones-blog.netlify.app',
+	},
+
+	{
+		id: 4,
+		image: IMG4,
+		title: 'Pig Game',
+		description: 'Pig Game using Javascript, HTML, and CSS',
+		github: 'https://github.com/Mac-Jones/pig-game-javascript',
+		demo: 'https://pig-game-mac-jones.netlify.app/',
 	},
 	{
 		id: 5,
@@ -46,13 +47,12 @@ export const portfolioData = [
 		github: 'https://github.com/Mac-Jones/forkify-app',
 		demo: 'https://recipe-app-macjones.netlify.app',
 	},
-
 	{
 		id: 6,
-		image: IMG6,
-		title: 'Portfolio Website Sample',
-		description: 'My personal portfolio website',
-		github: 'https://github.com/Mac-Jones/react-portfolio-website-mj',
-		demo: 'https://mac-jones-soriano.netlify.app',
+		image: IMG5,
+		title: 'Breakout',
+		description: 'Javascript Game using HTML, CSS and Javascript',
+		github: 'https://github.com/Mac-Jones/breakout-game-javascript',
+		demo: 'https://breakout-macjones.netlify.app/',
 	},
 ];
